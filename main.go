1fde9c2be4e303e616ef56a6a49bb98ec3d5fe54
Arun Shekar <shekar.arun@gmail.com>
2025-02-17 14:53:22 +0530

    feat: Add function to update first name

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ type person struct {
 func main() {
 	p1 := person{"Arun", "Shekar", 30, contactInfo{"a@a.com", 34}}
 	p1.print()
+	p1.updateFirstName("NewFirst")
+	p1.print()
 
 	p2 := person{firstName: "P2First", lastName: "P2Last", age: 35}
 	p2.print()
@@ -26,4 +28,9 @@ func main() {
 
 func (p person) print() {
 	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateFirstName(n string) {
+	p.firstName = n
+	fmt.Printf("First name updated to: %v\n", p.firstName)
 }
